f962b0488522c1da6ee7ac1ab140a52a1c479c4f
Alin <andreialin2324@yahoo.com>
2023-04-11 12:44:56 +0300

    add user Nickname

--- MobyLabWebProgramming.Core/DataTransferObjects/UserAddDTO.cs
+++ MobyLabWebProgramming.Core/DataTransferObjects/UserAddDTO.cs
@@ -8,6 +8,7 @@ namespace MobyLabWebProgramming.Core.DataTransferObjects;
 public class UserAddDTO
 {
     public string Name { get; set; } = default!;
+    public string Nickname { get; set; } = default!;
     public string Email { get; set; } = default!;
     public string Password { get; set; } = default!;
     public UserRoleEnum Role { get; set; } = default!;
